72bba76de222cdff912841c5a028bcdf7d5f3dda
Jacob MacDonald <jakemac@google.com>
2025-11-18 14:23:05 -0800

    add an expensive task widget for the cpu profiler page (#1)
    Includes fibonacci in the main isolate and a separate isolate.

--- lib/src/screens/cpu_profiler/cpu_profiler_screen.dart
+++ lib/src/screens/cpu_profiler/cpu_profiler_screen.dart
@@ -1,12 +1,32 @@
-import 'package:flutter/widgets.dart';
+import 'dart:isolate';
 
-import '../../shared/widgets/todo_screen.dart';
+import 'package:flutter/material.dart';
+
+import '../../shared/ui/theme.dart';
+import 'expensive_task_widget.dart';
 
 class CpuProfilerScreen extends StatelessWidget {
   const CpuProfilerScreen({super.key});
 
+  int _fib(int n) {
+    if (n <= 1) return n;
+    return _fib(n - 1) + _fib(n - 2);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const TodoScreen(screenName: 'CPU Profiler');
+    return ListView(
+      padding: const .all(largePadding),
+      children: [
+        ExpensiveTaskWidget(
+          title: 'Compute Fibonacci (37)',
+          task: () => _fib(37).toString(),
+        ),
+        ExpensiveTaskWidget(
+          title: 'Compute Fibonacci (37) in Isolate',
+          task: () => Isolate.run(() => _fib(37).toString()),
+        ),
+      ],
+    );
   }
 }
